d7220169e40eccd8aa42a254c2be08a71f4c18a7
ashish mahindrakar <ashishmahi46@gmail.com>
2018-01-12 14:47:07 +0530

    added tested model todoList

---
+++ test/todoListTest.js
@@ -0,0 +1,36 @@
+const ToDoList = require('../lib/todoList.js');
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('todoList', () => {
+    describe('adds a todoItem', () => {
+        it('should add a todoItem to users list', (done) => {
+            let todoList = new ToDoList("at Home","just home stuffs");
+            let todoItem = {text:"buy cloths"};
+            todoList.addItem(todoItem);
+            assert.exists(todoList.todoItems["buy cloths"])
+            done();
+        });
+    });
+    describe('removes a todoList', () => {
+        it('should remove a todolist from users list of todos ', (done) => {
+            let todoList = new ToDoList("at Home","just home stuffs");
+            let todoItem = {text:"buy cloths"};
+            todoList.addItem(todoItem);
+            todoList.removeTodoItem(todoItem);
+            assert.notExists(todoList.todoItems["at Home"]);
+            done();
+        });
+    });
+    describe('edit a title', () => {
+        it('should edit title of existing todo ', (done) => {
+            let todoList = new ToDoList("at Home","just home stuffs");
+            let todoItem = {text:"buy cloths"};
+            todoList.addItem(todoItem);
+            todoList.editTextOf("buy cloths","buy soap");
+            let replacedItem = todoList.todoItems["buy cloths"];
+            assert.notInclude(todoList.todoItems,replacedItem);
+            done();
+        });        
+    });
+});
