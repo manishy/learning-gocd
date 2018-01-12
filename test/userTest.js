db0aca4686dbb820adb254fe1a0c68bf77055852
ashish mahindrakar <ashishmahi46@gmail.com>
2018-01-12 14:17:27 +0530

    added a tested user class

---
+++ test/userTest.js
@@ -0,0 +1,45 @@
+const User = require('../lib/user.js');
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('user', () => {
+    describe('adds a todoList', () => {
+        it('should add a todo to users existing todos', (done) => {
+            let user = new User("ashish");
+            let newTodo = {
+                title:"at Home"
+            }
+            user.addTodo(newTodo);
+            let todos = user.todos["at Home"];
+            assert.include(todos,newTodo);
+            done();
+        });
+    });
+    describe('removes a todoList', () => {
+        it('should remove a todolist from users list of todos ', (done) => {
+            let user = new User("ashish");
+            let newTodo = {
+                title:"at Home"
+            }
+            user.addTodo(newTodo);
+            let todos = user.todos["at Home"];
+            assert.include(todos,newTodo);
+            user.removeTodo(newTodo);
+            assert.notExists(user.todos["at Home"]);
+            done();
+        });
+    });
+    describe('edit a title', () => {
+        it('should edit title of existing todo ', (done) => {
+            let user = new User("ashish");
+            let newTodo = {
+                title:"at Home"
+            }
+            user.addTodo(newTodo);
+            user.editTitleOf("at Home","at Office");
+            let todos = user.todos["at Home"];
+            assert.notInclude(user.todos,newTodo);
+            done();
+        });        
+    });
+});
