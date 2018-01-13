24b77b6aed0cdb621577ca0f5592d63ce45bc252
ashish mahindrakar <ashishmahi46@gmail.com>
2018-01-13 11:12:03 +0530

    fixed bug : made every addItem form seperate for every todoList

--- toHtml/toHtml.js
+++ toHtml/toHtml.js
@@ -16,10 +16,10 @@ exports.getToDoList = (todoTitle)=>{
     <br>
     <div id="${todoTitle}items">
     </div>
-    <div class="addToDoForm">
+    <div class="addToDoForm${todoTitle}">
     </div>
     <button id="${todoTitle}" onclick="showToDo(this.id)" name="button">view</button>
-    <button id="${todoTitle}" onclick="addToDoList()" name="button">addItem</button>
+    <button id="${todoTitle}" onclick="addToDoList(this.id)" name="button">addItem</button>
     <button id="${todoTitle}" onclick="editToDoList(this.id)" name="button">edit</button>
     <button id="${todoTitle}" onclick="deleteToDoList(this.id)" name="button">delete</button>
     </div>`;
